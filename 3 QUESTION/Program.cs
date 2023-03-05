44b03c8bdd45d798e158bb6f36aaa8e4f911f59a
Viktoria <vikusya25072000@gmail.com>
2023-03-05 20:31:24 +0300

    решила третью задачу

---
+++ 3 QUESTION/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 2 == 0)
+{
+    Console.Write("Введенное число является четным");
+    Console.Read();
+}
+ else
+{
+    Console.Write("Введенное число нечетное");
+    Console.Read();
+}
